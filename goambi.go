63aaba47f2dfa5dae246ae4e2b360ffb4bd2ec06
RandomByte <r@ndombyte.net>
2016-01-16 03:31:16 +0100

    Cleanup

--- goambi.go
+++ goambi.go
@@ -3,13 +3,16 @@ package main
 import (
 	dominantcolor "github.com/cenkalti/dominantcolor"
 	"image"
-	"image/color"
 	"image/jpeg"
 	"log"
 	"math"
 	"os"
 	"strconv"
 )
+
+type SubImager interface {
+	SubImage(r image.Rectangle) image.Image
+}
 
 func main() {
 	file, err := os.Open("_test/pic.jpg")
@@ -23,10 +26,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	screenRect := image.Rect(1350, 663, 2200, 975)
-	screen := img.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(screenRect)
+	screen := getScreen(img)
 
 	/*
 
@@ -47,54 +47,51 @@ func main() {
 	*/
 	var areas [6]image.Image
 
-	x0 := float64(screenRect.Dx() / 100)
+	bounds := screen.Bounds()
+	log.Println(bounds)
+
+	x0 := float64(bounds.Dx() / 100)
 	xA := int(math.Floor(x0 * 20))
 
-	y := int(math.Floor(float64(screenRect.Dy() / 2)))
-
-	bounds := screenRect
+	y := int(math.Floor(float64(bounds.Dy() / 2)))
 
 	areaRect := image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Min.X+xA, bounds.Min.Y+y)
-	areas[0] = screen.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(areaRect)
+	areas[0] = screen.(SubImager).SubImage(areaRect)
 
 	areaRect = image.Rect(bounds.Min.X, bounds.Min.Y+y, bounds.Min.X+xA, bounds.Max.Y)
-	areas[1] = screen.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(areaRect)
+	areas[1] = screen.(SubImager).SubImage(areaRect)
 
 	areaRect = image.Rect(bounds.Min.X+xA, bounds.Min.Y, bounds.Max.X-xA, bounds.Min.Y+y)
-	areas[2] = screen.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(areaRect)
+	areas[2] = screen.(SubImager).SubImage(areaRect)
 
 	areaRect = image.Rect(bounds.Min.X+xA, bounds.Min.Y+y, bounds.Max.X-xA, bounds.Max.Y)
-	areas[3] = screen.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(areaRect)
+	areas[3] = screen.(SubImager).SubImage(areaRect)
 
 	areaRect = image.Rect(bounds.Max.X-xA, bounds.Min.Y, bounds.Max.X, bounds.Min.Y+y)
-	areas[4] = screen.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(areaRect)
+	areas[4] = screen.(SubImager).SubImage(areaRect)
 
 	areaRect = image.Rect(bounds.Max.X-xA, bounds.Min.Y+y, bounds.Max.X, bounds.Max.Y)
-	areas[5] = screen.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(areaRect)
+	areas[5] = screen.(SubImager).SubImage(areaRect)
 
 	for i := 0; i < len(areas); i++ {
 		color := dominantcolor.Find(areas[i])
 		log.Println(i, color)
+		areas[i].Set(0, 0, color)
 
 		out, err := os.Create("_test/output-" + strconv.Itoa(i) + ".jpg")
 		if err != nil {
 			log.Fatal(err)
 		}
 		err = jpeg.Encode(out, areas[i], nil)
+
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func getScreen(img image.Image) image.Image {
+	screenRect := image.Rect(1350, 663, 2200, 975)
+	screen := img.(SubImager).SubImage(screenRect)
+	return screen
 }
